7edb1c84a3e11b39d52c5343349a8d7971ef41f0
emilio <emilio@satellogic.com>
2018-04-20 17:51:22 -0300

    Agregamos clase de wallet

--- simsat.py
+++ simsat.py
@@ -12,6 +12,23 @@ print('''
      :-----       /-\       -----:
                  /ooo\\
                 #|ooo|#
-                 \___/''')
+                 \___/'''
+
+class Wallet():
+    def __init__(self, initial_money, debug=0):
+        self.money = initial_money
+
+    def get_money(self):
+        return self.money
+
+    def set_money(self, money):
+        self.money = money
+
+    def show_me_the_money(self):
+        print(self.money)
+
+    def modify_money(self, change):
+        self.money += change
+
 
 
