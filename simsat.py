e9f29d8d26addcc8ccfb5e20457d9c89d264a232
emilio <emilio@satellogic.com>
2018-04-27 08:55:47 -0300

    agrego algo de bank

--- simsat.py
+++ simsat.py
@@ -18,9 +18,11 @@ sate = '''
                  \___/'''
 
 
+
 class Wallet():
     def __init__(self, initial_money, debug=0):
         self.money = initial_money
+        self.walletid=0
 
     def get_money(self):
         return self.money
@@ -35,11 +37,33 @@ class Wallet():
         self.money += change
 
 
-class Engine():
-    def __init__(self):
+class Engine(Wallet, Bank):
+    def __init__(self, wallet=Wallet(), initialtime, endtime):
+        currenttime = initialtime
+        
+        while (currenttime < endtime):
+            wallet = Bank.Fixed_term_deposit(wallet, 1000, endtime-initialtime)
+            
+            print ("t=", currenttime, "wallet="wallet.get_money)
+            
+            currenttime=currenttime+1
         pass
 
 
+class Bank(Wallet):
+    def __init__(self, debug=0):
+        self.minimun_fixed_term_time = 30 #30*24*60*60
+        self.minimun_fixed_term_money = 1000 #30*24*60*60
+        self.wallets = array[] #[id, status, money]
+        pass
+        
+    def Fixed_term_deposit(self, walletin = Wallet(), money = 0, time = minimun_fixed_term_time)
+        if (time >= minimun_fixed_term_time and money = minimun_fixed_term_money):
+            self.wallets.append ([walletin.walletid, 1, money])
+            walletin.set_money (walletin.get_money - money);
+        
+            
+        
 class TestWallet(unittest.TestCase):
     def setUp(self):
         self.wallet = Wallet(100)
