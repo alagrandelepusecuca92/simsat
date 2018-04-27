f28016ae15873294dba747d6041d50de684f986b
emilio <emilio@satellogic.com>
2018-04-27 17:37:03 -0300

    agrego tarea 2

---
+++ tests.py
@@ -0,0 +1,27 @@
+import unittest
+from simsat import Wallet
+
+class TestWallet(unittest.TestCase):
+    def setUp(self):
+        self.wallet = Wallet(100)
+
+    def test_default_money(self):
+        self.assertEqual(100, self.wallet.money)
+
+    def test_increase_money(self):
+        self.wallet.modify_money(10)
+        self.assertEqual(110, self.wallet.money)
+
+    def test_decrease_money(self):
+        self.wallet.modify_money(-30)
+        self.assertEqual(70, self.wallet.money)
+
+    def test_getter_money(self):
+        self.assertEqual(100, self.wallet.get_money())
+
+    def test_setter_money(self):
+        self.wallet.set_money(10)
+        self.assertEqual(10, self.wallet.get_money())
+
+if __name__ == '__main__':
+    unittest.main()
