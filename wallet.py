3a7a73446e79487e0d6bba73124cd0702c7304b8
Andres Miguel Airabella <tuk@satellogic.com>
2018-04-27 16:57:47 -0300

    Movemos la clase wallet a otro .py

---
+++ wallet.py
@@ -0,0 +1,15 @@
+class Wallet():
+    def __init__(self, initial_money, debug=0):
+        self.money = initial_money
+
+    def get_money(self):
+        return self.money
+
+    def set_money(self, money):
+        self.money = money
+
+    def show_me_the_money(self):
+        print(self.money)
+
+    def modify_money(self, change):
+        self.money += change
