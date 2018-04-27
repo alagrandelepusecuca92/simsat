dcaa0535ed77dd5a5d623038eed99e04f3285397
Andres Miguel Airabella <tuk@satellogic.com>
2018-04-27 17:15:34 -0300

    Agregamos el parser para poder poner un help, pasarle tiempo y plata.

--- simsat.py
+++ simsat.py
@@ -1,3 +1,11 @@
+import argparse
+import wallet
+
+def configure_parser(parser):
+    parser.add_argument("-t", "--time",  type=int, default=10,   help="Time in seconds (default: %(default)d)")
+    parser.add_argument("-m", "--money", type=int, default=1000, help="Money in bitcoins (default: %(default)d)")
+
+
 sate = '''
                 }--O--{
                   [^]
@@ -17,3 +25,8 @@ sate = '''
 class Engine():
     def __init__(self):
         pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="SimSAT Game")
+    configure_parser(parser)
+    args = parser.parse_args()
