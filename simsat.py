220d032d1c539ff485a51359b7414596b73dfbc8
emilio <emilio@satellogic.com>
2018-04-27 16:46:48 -0300

    agrego mas cosas de bank

--- simsat.py
+++ simsat.py
@@ -38,29 +38,45 @@ class Wallet():
 
 
 class Engine(Wallet, Bank):
-    def __init__(self, wallet=Wallet(), initialtime, endtime):
+    def __init__(self):
+        pass
+    
+    def start(self, wallet=Wallet(), initialtime, endtime):
         currenttime = initialtime
         
+        hacer_plazo_fijo = 1
+        
         while (currenttime < endtime):
-            wallet = Bank.Fixed_term_deposit(wallet, 1000, endtime-initialtime)
+            if (hacer_plazo_fijo == 1):
+                wallet = Bank.fixed_term_deposit(wallet, 1000, endtime-currenttime)
             
+            wallet
             print ("t=", currenttime, "wallet="wallet.get_money)
             
             currenttime=currenttime+1
-        pass
 
 
 class Bank(Wallet):
     def __init__(self, debug=0):
         self.minimun_fixed_term_time = 30 #30*24*60*60
         self.minimun_fixed_term_money = 1000 #30*24*60*60
-        self.wallets = array[] #[id, status, money]
+        self.wallets = [] #[id, status, money] status 1 depositada 2 para retirar
         pass
         
-    def Fixed_term_deposit(self, walletin = Wallet(), money = 0, time = minimun_fixed_term_time)
+    def fixed_term_deposit(self, walletin = Wallet(), money = 0, time = minimun_fixed_term_time)
         if (time >= minimun_fixed_term_time and money = minimun_fixed_term_money):
             self.wallets.append ([walletin.walletid, 1, money])
             walletin.set_money (walletin.get_money - money);
+    
+    def check_fixed_term_deposit(self, walletin = Wallet()):
+        for wa in self.wallets:
+            if (wa[0] == walletin.walletid):
+                if (wa[1] == 1):
+                    walletin.set_money = walletin.get_money + wa[2]
+                    walletin.remove(wa)
+   
+    def banktime(self,delta_time):
+        
         
             
         
